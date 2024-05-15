d0f673bbdc7c39bdd9a22a5e39a6ecb928a7d602
f-developer0507 <stormpubg0507@gmail.com>
2024-05-16 01:47:07 +0500

    initial commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,16 +6,16 @@ const Footer = () => {
       <div className="footer-row">
         <ul className="footer__list social-icons">
           <li className="footer__item">
-            <a href="#!" className="footer__link"></a>
+            <a href="https://ru-ru.facebook.com/" target="_blank" className="footer__link"></a>
           </li>
           <li className="footer__item">
-            <a href="#!" className="footer__link"></a>
+            <a href="https://www.instagram.com/" target="_blank" className="footer__link"></a>
           </li>
           <li className="footer__item">
-            <a href="#!" className="footer__link"></a>
+            <a href="https://twitter.com/" target="_blank" className="footer__link"></a>
           </li>
           <li className="footer__item">
-            <a href="#!" className="footer__link"></a>
+            <a href="https://www.linkedin.com/" target="_blank" className="footer__link"></a>
           </li>
         </ul>
         <p className="footer__copyright">Copyright ©2020 All rights reserved</p>
